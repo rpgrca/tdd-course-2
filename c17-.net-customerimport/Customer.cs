188d6af644e09a8ac403ff14479d8b5a179a0d72
Roberto Alfonso <roberto.alfonso@praxisemr.com>
2020-05-12 19:46:07 -0300

    initial commit

---
+++ c17-.net-customerimport/Customer.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate.Tool.hbm2ddl;
+
+
+namespace com.tenpines.advancetdd
+{
+
+    public class StoreConfiguration : DefaultAutomappingConfiguration
+    {
+        public override bool ShouldMap(Type type)
+        {
+            return type == typeof(Customer) || type == typeof(Address);
+        }
+    }
+
+    public class Address
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string StreetName { get; set; }
+        public virtual int StreetNumber { get; set; }
+        public virtual string Town { get; set; }
+        public virtual int ZipCode { get; set; }
+        public virtual string Province { get; set; }
+    }
+
+
+    public class Customer
+    {
+        public virtual long Id { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string IdentificationType { get; set; }
+        public virtual string IdentificationNumber { get; set; }
+        public virtual IList<Address> Addresses { get; set; }
+
+        public Customer()
+        {
+            Addresses = new List<Address>();
+        }
+
+        public virtual void AddAddress(Address anAddress)
+        {
+            Addresses.Add(anAddress);
+        }
+
+        public static void ImportCustomers()
+        {
+            var fileStream = new System.IO.FileStream("input.txt", FileMode.Open);
+
+            var storeConfiguration = new StoreConfiguration();
+            var configuration = Fluently.Configure()
+                .Database(MsSqlCeConfiguration.Standard.ShowSql().ConnectionString("Data Source=CustomerImport.sdf"))
+                .Mappings(m => m.AutoMappings.Add(AutoMap
+                    .AssemblyOf<Customer>(storeConfiguration)
+                    .Override<Customer>(map => map.HasMany(x => x.Addresses).Cascade.All())));
+
+            var sessionFactory = configuration.BuildSessionFactory();
+            new SchemaExport(configuration.BuildConfiguration()).Execute(true, true, false);
+            var session = sessionFactory.OpenSession();
+
+            var lineReader = new StreamReader(fileStream);
+
+            var transaction = session.BeginTransaction();
+            Customer newCustomer = null;
+            var line = lineReader.ReadLine();
+            while (line != null)
+            {
+                if (line.StartsWith("C"))
+                {
+                    var customerData = line.Split(',');
+                    newCustomer = new Customer();
+                    newCustomer.FirstName = customerData[1];
+                    newCustomer.LastName = customerData[2];
+                    newCustomer.IdentificationType = customerData[3];
+                    newCustomer.IdentificationNumber = customerData[3];
+                    session.Persist(newCustomer);
+                }
+                else if (line.StartsWith("A"))
+                {
+                    var addressData = line.Split(',');
+                    var newAddress = new Address();
+
+                    newCustomer.AddAddress(newAddress);
+                    newAddress.StreetName = addressData[1];
+                    newAddress.StreetNumber = int.Parse(addressData[2]);
+                    newAddress.Town = addressData[3];
+                    newAddress.ZipCode = int.Parse(addressData[4]);
+                    newAddress.Province = addressData[3];
+                }
+
+                line = lineReader.ReadLine();
+            }
+
+            transaction.Commit();
+            session.Close();
+
+            fileStream.Close();
+        }
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Customer.ImportCustomers();
+            }
+            catch (Exception e)
+            {
+                Console.Out.Write(e.Message);
+            }
+        }
+    }
+
+
+}
