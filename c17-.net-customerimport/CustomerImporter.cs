c8f140ff51e77a8a470e84abc4ac43c3402e8e9a
Roberto <rpgrca@gmail.com>
2020-05-13 19:54:17 -0300

    refactored import

--- c17-.net-customerimport/CustomerImporter.cs
+++ c17-.net-customerimport/CustomerImporter.cs
@@ -6,6 +6,9 @@ namespace com.tenpines.advancetdd
     {
         private readonly DataBase _dataBase;
         private readonly StreamReader _lineReader;
+        private string _currentLine;
+        private string[] _currentRecord;
+        private Customer _newCustomer;
 
         public CustomerImporter(DataBase dataBase, StreamReader lineReader)
         {
@@ -15,43 +18,81 @@ namespace com.tenpines.advancetdd
 
         public void Import()
         {
-            Customer newCustomer = null;
             _dataBase.BeginTransaction();
-            var line = _lineReader.ReadLine();
 
-            while (line != null)
+            InitializeImport();
+            while (ReadNextLine())
             {
-                if (line.StartsWith("C"))
-                {
-                    var customerData = line.Split(',');
-                    newCustomer = new Customer
-                    {
-                        FirstName = customerData[1],
-                        LastName = customerData[2],
-                        IdentificationType = customerData[3],
-                        IdentificationNumber = customerData[4]
-                    };
-                    _dataBase.Session.Persist(newCustomer);
-                }
-                else if (line.StartsWith("A"))
-                {
-                    var addressData = line.Split(',');
-                    var newAddress = new Address
-                    {
-                        StreetName = addressData[1],
-                        StreetNumber = int.Parse(addressData[2]),
-                        Town = addressData[3],
-                        ZipCode = int.Parse(addressData[4]),
-                        Province = addressData[5]
-                    };
-
-                    newCustomer.AddAddress(newAddress);
-                }
-
-                line = _lineReader.ReadLine();
+                CreateRecord();
+                ImportRecord();
             }
 
             _dataBase.EndTransaction();
+        }
+
+        private void InitializeImport()
+        {
+            _newCustomer = null;
+        }
+
+        private void ImportRecord()
+        {
+            if (IsCustomerRecord())
+            {
+                ImportCustomer();
+            }
+            else if (IsAddressRecord())
+            {
+                ImportAddress();
+            }
+        }
+
+        private void ImportAddress()
+        {
+            var newAddress = new Address
+            {
+                StreetName = _currentRecord[1],
+                StreetNumber = int.Parse(_currentRecord[2]),
+                Town = _currentRecord[3],
+                ZipCode = int.Parse(_currentRecord[4]),
+                Province = _currentRecord[5]
+            };
+
+            _newCustomer.AddAddress(newAddress);
+        }
+
+        private void ImportCustomer()
+        {
+            _newCustomer = new Customer
+            {
+                FirstName = _currentRecord[1],
+                LastName = _currentRecord[2],
+                IdentificationType = _currentRecord[3],
+                IdentificationNumber = _currentRecord[4]
+            };
+
+            _dataBase.Session.Persist(_newCustomer);
+        }
+
+        private bool ReadNextLine()
+        {
+            _currentLine = _lineReader.ReadLine();
+            return _currentLine != null;
+        }
+
+        private bool IsAddressRecord()
+        {
+            return _currentLine.StartsWith("A");
+        }
+
+        private bool IsCustomerRecord()
+        {
+            return _currentLine.StartsWith("C");
+        }
+
+        private void CreateRecord()
+        {
+            _currentRecord = _currentLine.Split(',');
         }
     }
 }
